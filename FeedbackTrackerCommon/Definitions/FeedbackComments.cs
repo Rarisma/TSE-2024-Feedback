db982a1bbc5a58b515d6271852c827cd74dcf41d
finob2805 <162075174+finob2805@users.noreply.github.com>
2024-11-04 02:20:18 +0000

    Update FeedbackComments.cs

--- FeedbackTrackerCommon/Definitions/FeedbackComments.cs
+++ FeedbackTrackerCommon/Definitions/FeedbackComments.cs
@@ -4,27 +4,27 @@ namespace FeedbackTrackerCommon.Definitions;
 
 public class FeedbackComments
 {
-	/// <summary>
-	/// ID of feedback comment in database.
-	/// </summary>
-	[Key]
-	public int CommentID { get; set; }
+    /// <summary>
+    /// ID of feedback comment in database.
+    /// </summary>
+    [Key]
+    public int CommentID { get; set; }
 
-	/// <summary>
-	/// Content of comment
-	/// </summary>
-	public string Body { get; set; }
+    /// <summary>
+    /// Content of comment
+    /// </summary>
+    public string Body { get; set; }
 
-	/// <summary>
-	/// Account ID of commenter
-	/// </summary>
-	public int CommenterID { get; set; }
+    /// <summary>
+    /// Account ID of commenter
+    /// </summary>
+    public int CommenterID { get; set; }
 
     /// <summary>
     /// Feedback thread this comment belongs to
     /// </summary>
     public int FeedbackID { get; set; }
 
-	public User Commenter { get; set; }
-	public Feedback FeedbackC { get; set; }
+    public User Commenter { get; set; }
+    public Feedback FeedbackC { get; set; }
 }
