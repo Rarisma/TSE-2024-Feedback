e549a54db5a407cf0e08e0b6fccf61f419999d85
finob2805 <162075174+finob2805@users.noreply.github.com>
2024-11-04 02:19:37 +0000

    Update Feedback.cs

--- FeedbackTrackerCommon/Definitions/Feedback.cs
+++ FeedbackTrackerCommon/Definitions/Feedback.cs
@@ -4,48 +4,48 @@ namespace FeedbackTrackerCommon.Definitions;
 
 public class Feedback
 {
-	/// <summary>
-	/// ID of feedback in database.
-	/// </summary>
-	[Key]
-	public int FeedbackID { get; set; }
+    /// <summary>
+    /// ID of feedback in database.
+    /// </summary>
+    [Key]
+    public int FeedbackID { get; set; }
 
-	/// <summary>
-	/// Person recieiving feedback
-	/// </summary>
-	public int AssignedUserID { get; set; }
+    /// <summary>
+    /// Person recieiving feedback
+    /// </summary>
+    public int AssignedUserID { get; set; }
 
-	/// <summary>
-	/// Person giving the feedback
-	/// </summary>
-	public int AssigneeID { get; set; }
+    /// <summary>
+    /// Person giving the feedback
+    /// </summary>
+    public int AssigneeID { get; set; }
 
     /// <summary>
     /// Associated Module
     /// </summary>
     public int ModuleID { get; set; }
 	
-	/// <summary>
-	/// Body of feedback
-	/// </summary>
-	public string FeedbackText { get; set; }
+    /// <summary>
+    /// Body of feedback
+    /// </summary>
+    public string FeedbackText { get; set; }
 
-	/// <summary>
-	/// Is the feedback open or closed
-	/// </summary>
-	public bool Closed { get; set; }
+    /// <summary>
+    /// Is the feedback open or closed
+    /// </summary>
+    public bool Closed { get; set; }
 	
-	/// <summary>
-	/// Feedback title
-	/// </summary>
-	public string Title { get; set; }
+    /// <summary>
+    /// Feedback title
+    /// </summary>
+    public string Title { get; set; }
 
-	/// <summary>
-	/// Priority Levels
-	/// </summary>
-	public int Priority { get; set; }
+    /// <summary>
+    /// Priority Levels
+    /// </summary>
+    public int Priority { get; set; }
 
-	public User AssignedUser { get; set; }
-	public User Assignee { get; set; }
-	public Modules Module { get; set; }
+    public User AssignedUser { get; set; }
+    public User Assignee { get; set; }
+    public Modules Module { get; set; }
 }
