d85f453abf98a5a4b218c497117ec08dcd5c7d81
finob2805 <162075174+finob2805@users.noreply.github.com>
2024-11-04 02:17:37 +0000

    Update User.cs

--- FeedbackTrackerCommon/Definitions/User.cs
+++ FeedbackTrackerCommon/Definitions/User.cs
@@ -4,26 +4,26 @@ namespace FeedbackTrackerCommon.Definitions;
 
 public class User
 {
-	/// <summary>
-	/// Users account ID
-	/// </summary>
-	[Key]
+    /// <summary>
+    /// Users account ID
+    /// </summary>
+    [Key]
     public int UserID { get; set; }
 
-	/// <summary>
-	/// User's username
-	/// </summary>
+    /// <summary>
+    /// User's username
+    /// </summary>
     public string Username { get; set; }
 
-	public string Password { get; set; }
+    public string Password { get; set; }
     
-	/// <summary>
-	/// Is the user a student
-	/// </summary>
-	public bool IsStudent { get; set; }
-
-	/// <summary>
-	/// Is the student a teacher
-	/// </summary>
+    /// <summary>
+    /// Is the user a student
+    /// </summary>
+    public bool IsStudent { get; set; }
+	
+    /// <summary>
+    /// Is the student a teacher
+    /// </summary>
     public bool IsTeacher { get; set; }
 }
