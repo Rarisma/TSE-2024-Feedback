eca13ffdf3eac2caf15644ef754901ad976d5cc9
kokowoo <Joshilee123123@gmail.com>
2024-11-26 14:55:07 +0000

    feat:  Create feedback form creates feedback in db

--- FeedbackTrackerCommon/Definitions/User.cs
+++ FeedbackTrackerCommon/Definitions/User.cs
@@ -1,6 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+namespace FeedbackTrackerCommon.Definitions;
+
 public class User
 {
 	/// <summary>
