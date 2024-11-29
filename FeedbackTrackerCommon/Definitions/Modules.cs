e5e5911a64f64ea6b8f35256e12e5180d93f8f3a
kokowoo <Joshilee123123@gmail.com>
2024-11-29 01:24:41 +0000

    feat: starter module API

--- FeedbackTrackerCommon/Definitions/Modules.cs
+++ FeedbackTrackerCommon/Definitions/Modules.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FeedbackTrackerCommon.Definitions;
 
 public class Modules
 {
     [Key]
+    [Column("MODULE_ID")]
     public int ModuleID { get; set; }
+    [Column("MODULE")]
     public string Module { get; set; }
 
 }
---
+++ Server/API/ModuleController.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using FeedbackTrackerCommon.Definitions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+
+namespace Server.API;
+[ApiController]
+[Route("Module")]
+public class ModuleController : Controller
+{
+    /// <summary>
+    /// Get Module by ID
+    /// </summary>
+    /// <param name="ID">Module ID</param>
+    /// <return>Module Object</return>
+    [HttpGet("GetModuleByID")]
+    public string GetModuleByID(int ID)
+    {
+        try
+        {
+            //Find account
+            using TrackerContext Ctx = new();
+            Modules module = Ctx.modules.First(module => module.ModuleID == ID);
+
+            //Serialise to JSON
+            string json = JsonSerializer.Serialize(module);
+            return json;
+        }
+        catch (Exception ex) { return "Encountered an error: " + ex.Message; }
+    }
+
+    /// <summary>
+    /// Get Module by ID
+    /// </summary>
+    /// <param name="ID">Module ID</param>
+    /// <return>Module Object</return>
+    [HttpGet("GetModuleByName")]
+    public string GetModuleByName(string Name)
+    {
+        try
+        {
+            //Find account
+            using TrackerContext Ctx = new();
+            Modules module = Ctx.modules.First(module => module.Module == Name);
+
+            //Serialise to JSON
+            string json = JsonSerializer.Serialize(module);
+            return json;
+        }
+        catch (Exception ex) { return "Encountered an error: " + ex.Message; }
+    }
+}
