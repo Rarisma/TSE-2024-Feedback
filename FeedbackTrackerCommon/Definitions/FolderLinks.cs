863bbddc792c9ab4b5e8b6c018eea440e628c7b8
finob2805 <162075174+finob2805@users.noreply.github.com>
2024-12-10 20:47:13 +0000

    Update FolderLinks.cs

--- FeedbackTrackerCommon/Definitions/FolderLinks.cs
+++ FeedbackTrackerCommon/Definitions/FolderLinks.cs
@@ -1,13 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FeedbackTrackerCommon.Definitions;
 
 public class FolderLinks
 {
-	  [Key]
-	  public int FolderID { get; set; }
-    public int FeedbackID { get; set; }
+	[Key]
+	[Column("FOLDER_ID")]
+	public int FolderID { get; set; }
+
+	[Column("FEEDBACK_ID")]
+	public int FeedbackID { get; set; }
 
     public FeedbackFolders FeedbackFolder { get; set; }
     public Feedback Feedback { get; set; }
