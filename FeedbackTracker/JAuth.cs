7b77223e19b895cb5436947d951e283126404c9d
Jake <49795711+Rarisma@users.noreply.github.com>
2025-02-21 09:47:21 +0000

    Fix login issue

--- FeedbackTracker/JAuth.cs
+++ FeedbackTracker/JAuth.cs
@@ -15,10 +15,10 @@ namespace Application;
 /// </summary>
 public class JAuth(NavigationManager NavigationManager)
 {
-	public static User User;
-	private static ClaimsPrincipal? UserData;
-	private static string JWT;
-
+	public User User;
+	private ClaimsPrincipal? UserData;
+	private string JWT;
+	
 	/// <summary>
 	/// Authorises a user.
 	/// </summary>
@@ -108,9 +108,11 @@ public class JAuth(NavigationManager NavigationManager)
 		{
 			Log.Error(ex, "Error enforcing authentication");
 		}
+		
+		//Send to login since user unauthorised
 		try
 		{
-		NavigationManager.NavigateTo("/LogIn");
+			NavigationManager.NavigateTo("/LogIn");
 		}
 		catch (Exception ex)
 		{
@@ -118,12 +120,10 @@ public class JAuth(NavigationManager NavigationManager)
         }
 	}
 
-	public string GetToken()
-	{
-		return JWT;
-	}
-	public User GetUser()
-	{
-		return User;
-	}
+	/// <summary>
+	/// Helper function to get User object.
+	/// </summary>
+	public User GetUser() => User;
+
+	public string GetToken() => JWT;
 }
