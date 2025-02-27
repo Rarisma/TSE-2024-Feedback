4e92419cff068939e5d737affcb1b1b9ebbc3d94
finob2805 <162075174+finob2805@users.noreply.github.com>
2025-02-27 23:59:13 +0000

    Update Feedback.cs

--- FeedbackTrackerCommon/Definitions/Feedback.cs
+++ FeedbackTrackerCommon/Definitions/Feedback.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FeedbackTrackerCommon.Definitions;
 
-public clasms Feedback
+public class Feedback
 {
     /// <summary>
     /// ID of feedback in database.
