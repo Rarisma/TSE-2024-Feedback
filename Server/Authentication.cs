f16ad3ee060c1d8f75d2bd39d8cb93189817c51f
Kuba <87782820+MonLiFl@users.noreply.github.com>
2025-04-01 11:36:32 +0100

    Fixed indentation

--- Server/Authentication.cs
+++ Server/Authentication.cs
@@ -41,13 +41,13 @@ public class AuthService(IConfiguration configuration, TrackerContext context)
 			}
 		}
 		
-		//Update login time.
+			//Update login time.
 		user.LastLogin = DateTime.Now;
 		context.User.Update(user);
-        await context.SaveChangesAsync();
+		await context.SaveChangesAsync();
 
-        // Generate JWT token
-        string token = GenerateJwtToken(user);
+		// Generate JWT token
+		string token = GenerateJwtToken(user);
 		return token;
 	}
 
