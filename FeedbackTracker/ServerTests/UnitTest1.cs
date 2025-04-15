7668c3194a14b0551c3dfb589f85a32ef070e2f0
Kuba <87782820+MonLiFl@users.noreply.github.com>
2025-04-15 13:27:08 +0100

    Couple tests for review
    Unit tests for jake to review

--- FeedbackTracker/ServerTests/UnitTest1.cs
+++ FeedbackTracker/ServerTests/UnitTest1.cs
@@ -49,6 +49,7 @@ public class FeedbackControllerTests
         Feedback testFeedback = new Feedback
         {
             Title = Guid.NewGuid().ToString(), //GUIDs are globally unique
+            FeedbackID = 1,
             FeedbackText = "TestDescription",
             Label = FeedbackLabel.Question,
             AssigneeID = 1,
@@ -72,4 +73,131 @@ public class FeedbackControllerTests
         //Assert our result is a success.
         Assert.IsFalse(ctx.Feedback.Contains(testFeedback), "Feedback not cleaned up.");
     }
+
+
+    /// <summary>
+    /// Checks if the feedback ID of a Feedback is invalid
+    /// </summary>
+    [TestMethod]
+    public void CheckInvalidFeebackID()
+    {
+        // Call function with invalid feedback ID
+        var result = _controller.GetFeedbackByID(-9999);
+
+        // Assert our result is an error.
+        Assert.IsTrue(result.Contains("Invalid feedback ID"));
+    }
+
+
+
+    /// <summary>
+    /// This hits the GetFeedbackById  endpoint
+    /// </summary>
+    [TestMethod]
+    public void CheckValidFeebackID()
+    {
+        Feedback testFeedback = new Feedback
+        {
+            Title = Guid.NewGuid().ToString(), //GUIDs are globally unique
+            FeedbackID = 999,
+            FeedbackText = "TestDescription",
+            Label = FeedbackLabel.Question,
+            AssigneeID = 1,
+            AssignedUserID = 1,
+            Closed = false,
+            ModuleID = 1,
+
+        };
+
+        // Create Feedback
+        _controller.CreateFeedback(testFeedback);
+
+        // Get feedback 
+        var result = _controller.GetFeedbackByID(testFeedback.FeedbackID);
+
+        // Test if its the right feedback
+        Assert.IsTrue(result.Contains(testFeedback.Title), "Feedback doesnt include the title.");
+
+        // Clean up
+        using TrackerContext ctx = new();
+
+        //Clean up so we don't clutter the db.
+        ctx.Feedback.Remove(testFeedback);
+        ctx.SaveChanges();
+
+        //Assert our result is a success.
+        Assert.IsFalse(ctx.Feedback.Contains(testFeedback), "Feedback not cleaned up.");
+    }
+
+    /// <summary>
+    /// Checks if the feedback ID of a Feedback is invalid
+    /// </summary>
+    [TestMethod]
+    public void GetPublicFeedbacksInvalid()
+    {
+        // Call function with invalid feedback ID
+        var result = _controller.GetPublicFeedbacks(null);
+
+        // Assert our result is an error.
+        Assert.IsTrue(result.Contains("Invalid feedback ID"));
+    }
+
+    /// <summary>
+    /// Tests feedback visibility
+    /// </summary>
+    [TestMethod]
+    public void TestFeebackVisibility()
+    {
+        Feedback publicfeedback = new Feedback
+        {
+            Title = Guid.NewGuid().ToString(), //GUIDs are globally unique
+            FeedbackText = "TestDescription",
+            Label = FeedbackLabel.Question,
+            AssigneeID = 1,
+            AssignedUserID = 1,
+            Closed = false,
+            ModuleID = 1,
+            Visibility = FeedbackVisibility.Public
+
+        };
+        Feedback privatefeedback = new Feedback
+        {
+            Title = Guid.NewGuid().ToString(), //GUIDs are globally unique
+            FeedbackText = "TestDescription2",
+            Label = FeedbackLabel.Question,
+            AssigneeID = 2,
+            AssignedUserID = 2,
+            Closed = false,
+            ModuleID = 1,
+            Visibility = FeedbackVisibility.Private
+
+        };
+
+
+        // Create Feedback
+        _controller.CreateFeedback(publicfeedback);
+        _controller.CreateFeedback(privatefeedback);
+
+
+
+        // Call function with valid feedback
+        string result = _controller.GetPublicFeedbacks();
+
+        // Tests if it only finds public feedbacks
+        Assert.IsTrue(result.Contains(publicfeedback.Title), "Contains public feedback");
+        Assert.IsFalse(result.Contains(privatefeedback.Title), "Doesnt contain private feedback");
+
+        //Clean up so we don't clutter the db.
+        using TrackerContext ctx = new();
+
+        ctx.Feedback.Remove(publicfeedback);
+        ctx.Feedback.Remove(privatefeedback);
+        ctx.SaveChanges();
+
+        //Assert our result is a success.
+        Assert.IsFalse(ctx.Feedback.Contains(publicfeedback), "Feedback not cleaned up.");
+        Assert.IsFalse(ctx.Feedback.Contains(privatefeedback), "Feedback not cleaned up.");
+
+    }
+
 }
